a0366d2ada45c6a4a3939e57abf93b103ddb7000
mrseoliveira <eva.oliveira@gmail.com>
2021-03-10 10:38:23 +0000

    testing

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent  {
+
+emotions =[
+  {name:"happy"},
+  {name:"Melancholic"},
+  {name:"Optimistic"},
+  {name:"Inspired"},
+  {name:"Concerned"}
+]
+
+
+movies = [
+  {title:"Lost in Translation"},
+  {title:"Cashback"},
+  {title:"A Single Man"},
+  {title:"Happy Go Lucky"},
+  {title:"Cinema Paradiso"}
+]
+ 
+  constructor() {
+   }
+
+
+
+   classifyEmotion(values){
+    console.log('ngValue',values)
+  }
+
+
+
+}
