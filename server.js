39e81fbd7a3b4952605651da8ab137630357db6e
Eva Oliveira <eva.oliveira@gmail.com>
2021-04-07 17:32:40 +0100

    server.js

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const path = __dirname + "/app/views/";
-
+const dbConfig = require("./backend/config/db.config.js");
 const app = express();
 
-app.use(express.static(path));
-
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:4200/",
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
+app.use(express.json());
+app.use(express.urlencoded());
 
-db.sequelize.sync();
-
-app.get("/", function (req, res) {
-  res.sendFile(path + "index.html");
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to our backend application." });
 });
-
-require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
