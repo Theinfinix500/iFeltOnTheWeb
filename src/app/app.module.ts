6a0064477bbf2aa514e946019f368c94033f8757
Eva Oliveira <eva.oliveira@gmail.com>
2021-04-08 08:36:31 +0100

    alert v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { MoviesService } from './components/movie.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ChangeDetectionStrategy } from '@angular/core';
-import {FormsModule} from '@angular/forms'
-import { AppRoutingModule } from './app-routing.module';
+import { MoviesService } from "./components/movie.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ChangeDetectionStrategy } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AlertModule } from "./components/_alert/alert.module";
 
-import { AppComponent } from './app.component';
-import {HomeComponent} from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MovieComponent } from './components/movies/list/movie.component';
-import { CreateMovieComponent } from './components/movies/create/createMovie.component'
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MovieComponent } from "./components/movies/list/movie.component";
+import { CreateMovieComponent } from "./components/movies/create/createMovie.component";
 
 @NgModule({
   declarations: [
@@ -16,14 +17,10 @@ import { CreateMovieComponent } from './components/movies/create/createMovie.com
     HomeComponent,
     HeaderComponent,
     MovieComponent,
-    CreateMovieComponent
+    CreateMovieComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AlertModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Movie } from "../models/movie.model";
+import { Emotions } from "../models/emotions";
+
+import { MoviesService } from "./../movie.service";
+import { AlertService } from "../_alert/alert.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  moviesUpdated: Subscription;
+  movies: Movie[] = [];
+  emotions: Emotions[] = [];
 
-  constructor() { }
+  constructor(
+    public movieService: MoviesService,
+    protected alertService: AlertService
+  ) {}
+
+  options = {
+    autoClose: true,
+    keepAfterRouteChange: false,
+  };
 
   ngOnInit() {
-    
-  }
+    // this.movies = this.movieService.getMovies();
+    // console.log(this.movies)
 
+    this.moviesUpdated = this.movieService
+      .getMoviesUpdateListener()
+      .subscribe((moviesReturned: Movie[]) => {
+        // debugger;
+        this.movies = moviesReturned;
+        // console.log('subscriptor HOME',this.movies)
+      });
+
+    this.emotions = this.movieService.getEmotions();
+  }
 }
