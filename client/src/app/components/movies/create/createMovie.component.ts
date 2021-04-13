e03d8f38a8b80f3f62499be56f1716b7d53b6758
Eva Oliveira <eva.oliveira@gmail.com>
2021-04-13 06:49:13 +0100

    form invalid

--- client/src/app/components/movies/create/createMovie.component.ts
+++ client/src/app/components/movies/create/createMovie.component.ts
@@ -1,42 +1,37 @@
-import { Router} from '@angular/router';
-import { MoviesService } from './../../movie.service';
-import { NgForm } from '@angular/forms';
-import { Component, OnInit ,ChangeDetectionStrategy} from '@angular/core';
+import { Router } from "@angular/router";
+import { MoviesService } from "./../../movie.service";
+import { NgForm } from "@angular/forms";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
-
-import { Movie } from './../../models/movie.model';
-
+import { Movie } from "./../../models/movie.model";
 
 @Component({
-  selector: 'app-createMovie',
-  templateUrl: './createMovie.component.html',
-  styleUrls: ['./createMovie.component.css']
+  selector: "app-createMovie",
+  templateUrl: "./createMovie.component.html",
+  styleUrls: ["./createMovie.component.css"],
 })
-export class CreateMovieComponent{
+export class CreateMovieComponent {
+  constructor(public moviesService: MoviesService, private router: Router) {}
 
-   constructor(public moviesService:MoviesService, private router:Router) { }
+  onMovieCreate(form: NgForm) {
+    if (form.invalid) {
+      alert("inavalid");
+    }
+    const movie: Movie = {
+      title: form.value.title,
+      file: form.value.file,
+      direction: form.value.direction,
+      year: form.value.year,
+      country: form.value.country,
+      duration: form.value.duration,
+      cast: {
+        name1: form.value.name1,
+        name2: form.value.name2,
+      },
+    };
 
-  onMovieCreate(form:NgForm){
-    // console.log(form.value)
-    
-     const movie:Movie = {
-      title:form.value.title,
-      file:form.value.file,
-      direction:form.value.direction,
-      year:form.value.year,
-      country:form.value.country,
-      duration:form.value.duration,
-      cast:{
-          name1:form.value.name1,
-          name2:form.value.name2
-          }
-      }
-
-    
-  this.moviesService.addMovie(movie)
-  // console.log("onMovieCreate",this.moviesService.addMovie(movie))
-  this.router.navigate(['/home'])
-
-}
-
+    this.moviesService.addMovie(movie);
+    // console.log("onMovieCreate",this.moviesService.addMovie(movie))
+    this.router.navigate(["/home"]);
+  }
 }
