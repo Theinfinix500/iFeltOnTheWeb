8acb67c077bff269e145234d20e1c3a8ab863b83
Eva Oliveira <eva.oliveira@gmail.com>
2021-04-21 15:53:22 +0100

    working

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/api/movies", (req, res, next) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
